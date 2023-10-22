2e5b13128984f40571b918bffbcd2a23d7da9d23
EVMlord <85406816+EVMlord@users.noreply.github.com>
2023-10-22 14:33:36 +0100

    Add toro testnet CA

--- src/hooks/addresses.ts
+++ src/hooks/addresses.ts
@@ -2,5 +2,6 @@ export const addresses = {
     CofferCityVault: {
         56: "",
         97: "0x3c0Fa6A8E8852185720f12587cE4F840f37f14c2",
+        54321: "0x96ad2b94e8C032E500663b8014A0BB7450F1263e",
     },
 }
